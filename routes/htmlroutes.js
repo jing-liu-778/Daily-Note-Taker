54f34fcf3ae98ea0e835c7369fefa53a34817b00
Jing Liu <jingliu778@gmail.com>
2022-01-21 15:19:05 -0500

    create routes

---
+++ routes/htmlroutes.js
@@ -0,0 +1,22 @@
+// Dependencies
+const path = require("path");
+
+// Routing
+
+module.exports = function (app){
+
+
+    //get route for homepage
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/pages/index.html"));
+});
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/pages/index.html'));
+});
+//get route for note page 
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/pages/notes.html"));
+})
+
+};
+
---
+++ server.js
@@ -0,0 +1,23 @@
+
+
+// Dependencies
+const express = require("express");
+
+// Express configuration
+//Tells node that we are creating an 'express' server
+const app = express();
+
+// Sets an initial port.
+const PORT = process.env.PORT || 3001;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// ROUTES
+require('./routes/apiroutes')(app);
+require('./routes/htmlroutes')(app);
+
+app.listen(PORT, function() {
+    console.log(`App listening on PORT: ${PORT}`)})
