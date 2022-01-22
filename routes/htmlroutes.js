d34f871b30f74bb93303f898c0deae92776396f7
Jing Liu <jingliu778@gmail.com>
2022-01-21 19:36:44 -0500

    update folder

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -4,13 +4,13 @@ const router = require('express').Router();
 
     //get route for homepage
 router.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, '../public/pages/index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 //get route for note page 
 router.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, '../public/pages/notes.html'));
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
 })
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/pages/index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 module.exports = router;
